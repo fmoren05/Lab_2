354aa392264764c8d8ce133c2ffb55dd6a65294d
fmoren05 <156385950+fmoren05@users.noreply.github.com>
2024-02-16 13:34:25 -0800

    deleted old docs files

--- docs/annotated_dup.js
+++
@@ -1,6 +0,0 @@
-var annotated_dup =
-[
-    [ "main", null, [
-      [ "Encoder", "classmain_1_1_encoder.html", "classmain_1_1_encoder" ]
-    ] ]
-];
--- docs/classmain_1_1_encoder.js
+++
@@ -1,6 +0,0 @@
-var classmain_1_1_encoder =
-[
-    [ "__init__", "classmain_1_1_encoder.html#ae37dece4dfb2aadd379129bf7b6c8c45", null ],
-    [ "read", "classmain_1_1_encoder.html#a109bc6c4966d7ad726a25eeb47be77d0", null ],
-    [ "zero", "classmain_1_1_encoder.html#a6ba71fa08f3a4c845e61760b83bb70c7", null ]
-];
