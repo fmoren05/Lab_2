653f97166da5bc126139cbebcbeb5c9306ce7f36
fmoren05 <156385950+fmoren05@users.noreply.github.com>
2024-02-16 13:51:24 -0800

    updates

---
+++ docs/docs/navtreeindex0.js
@@ -0,0 +1,13 @@
+var NAVTREEINDEX0 =
+{
+"annotated.html":[0,0],
+"classencoder__reader_1_1_encoder.html":[0,0,0,0],
+"classencoder__reader_1_1_encoder.html#a0b5ad086c21a7c4c0fdcc7977b596092":[0,0,0,0,1],
+"classencoder__reader_1_1_encoder.html#a4f0013ab8cd7010afd07096840604a03":[0,0,0,0,2],
+"classencoder__reader_1_1_encoder.html#a7950ee4f2284da277b88dfb5a825ae35":[0,0,0,0,0],
+"classes.html":[0,1],
+"functions.html":[0,2,0],
+"functions_func.html":[0,2,1],
+"index.html":[],
+"pages.html":[]
+};
