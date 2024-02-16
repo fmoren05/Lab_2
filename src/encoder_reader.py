ed607b224f7ebd3602048ce18677e76ae6314dde
fmoren05 <156385950+fmoren05@users.noreply.github.com>
2024-02-16 13:26:58 -0800

    Update

--- src/encoder_reader.py
+++ src/encoder_reader.py
@@ -1,11 +1,19 @@
-"Authors: Connor Schott, Fermin Moreno, Berent Baysal"
+# Authors: Connor Schott, Fermin Moreno, Berent Baysal
+# Class: Encoder
+# Data: 16th February 2024
+# Brief Description: This class implements an encoder reader using Pyboard's Timer and Pin modules.
 
 from pyb import Timer, Pin
+import time
+
+
 
 class Encoder:
     """!
     This class implements an encoder reader.
     """
+    
+    
 
     def __init__(self, timer, enc_pin_A, enc_pin_B):
         """!
@@ -17,72 +25,53 @@ class Encoder:
         self.timer = Timer(timer, prescaler=0, period=0xFFFF)
         self.enc_chA = self.timer.channel(1, Timer.ENC_AB, pin=enc_pin_A)
         self.enc_chB = self.timer.channel(2, Timer.ENC_AB, pin=enc_pin_B)
-
-        self.encoder_value1 = None  # Initialize encoder value
-        self.encoder_value2 = None  # Initialize encoder value
-        
-        self.prev_chA_value = self.enc_chA.capture()  # Store previous channel A value
-        self.prev_chB_value = self.enc_chB.capture()  # Store previous channel B value
+        self.cur_value = 0
+        self.prev_value = self.timer.counter()
         
         print("Creating an encoder reader")
-
-    def zero(self):
-        """
-        Sets the count to zero at the current position for both encoders.
-        """
-        if self.encoder_value1 is None:
-            self.encoder_value1 = 0
         
-        if self.encoder_value2 is None:
-            self.encoder_value2 = 0
+        
 
     def read(self):
         """!
         This method reads the encoder values.
         @return: Tuple containing the current encoder count, delta, and channel A and B values
         """
-        # Capture current channel A and B values
-        curr_chA_value = self.enc_chA.capture()
-        curr_chB_value = self.enc_chB.capture()
 
         # Calculate delta based on channel A value change
-        delta1 = curr_chA_value - self.prev_chA_value
-        delta2 = curr_chB_value - self.prev_chB_value
+        delta = self.timer.counter() - self.prev_value
 
         # Handle overflow and underflow conditions
-        if delta1 > 32767:
-            delta1 -= 65536
-        elif delta1 < -32768:
-            delta1 += 65536
-
-        # Update encoder value
-        if self.encoder_value1 is None:
-            self.encoder_value1 = delta1
-        else:
-            self.encoder_value1 += delta1
+        if delta > 32767:
+            delta -= 65536
+        elif delta < -32768:
+            delta += 65535
         
-        if delta2 > 32767:
-            delta2 -= 65536
-        elif delta2 < -32768:
-            delta2 += 65536
-        
-        if self.encoder_value2 is None:
-            self.encoder_value2 = delta2
-        else:
-            self.encoder_value2 += delta2
-
         # Store current values for next iteration
-        self.prev_chA_value = curr_chA_value
-        self.prev_chB_value = curr_chB_value
+        self.prev_value = self.timer.counter()
+        self.cur_value += delta
+        time.sleep(.1)
+        
+        return self.cur_value
 
-        return self.encoder_value1, self.encoder_value2
+
+
+    def zero(self):
+        """
+        Sets the count to zero at the current position for both encoders.
+        """
+        self.cur_value = 0
+        
+        
 
 if __name__ == '__main__':
+    
     # Specify encoder pins and timers for encoder1 and encoder2
     encoder1 = Encoder(4, Pin.board.PB6, Pin.board.PB7)
     encoder2 = Encoder(8, Pin.board.PC6, Pin.board.PC7)
     
     while True:
+        
         # Read encoder1 values
         encoder1_value = encoder1.read()
         
@@ -95,4 +84,3 @@ if __name__ == '__main__':
         print()
         print("Encoder 2 Value:", encoder2_value)
         print()
-        pyb.delay(50)  # Adjust delay for higher precision
